52d2531585140f0364e2ae2bc11fe97bbd423d5f
Cwills <ampocoll.wills@gmail.com>
2025-07-13 20:52:28 -0800

    Edit args file

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,5 +2,5 @@ const sayName = function Myname(name, time, age) {
     console.log(`Good ${time} ${name} you are ${age} years old`);
 };
 sayName();
-sayName('Collins');
-sayName('Collins', 'Morning', '20');
+// sayName('Collins');
+// sayName('Collins', 'Morning', '20');
